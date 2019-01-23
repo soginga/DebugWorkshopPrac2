b6c1b157f130b2b610fb0d0568e2efe8a84a6969
Derik Breedt <dbreedt@retrorabbit.co.za>
2019-01-23 23:45:25 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Globalization;
+
+namespace debugws2
+{
+  class Program
+  {
+    private long sum()
+    {
+      string[] data = File.ReadAllLines("data");
+      long total = 0;
+
+      for (int x = 0; x < data.Length; x++)
+      {
+        total += cnv(data[x]);
+      }
+
+      return total;
+    }
+
+    private int cnv(string val)
+    {
+      Int32.TryParse(val, NumberStyles.HexNumber, CultureInfo.InvariantCulture, out int value);
+
+      return value;
+    }
+
+    static void Main(string[] args)
+    {
+      var p = new Program();
+      Console.WriteLine("Total: {0}", p.sum());
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+# DebugWorkshopPrac2
+## How to build
+```bash
+$ dotnet build
+```
+
+## How to run
+```
+# dotnet run
+```
+
+## How to solve
+A before B
+C before D
+sam for 1, 2 and 3
+but what rhymes with G
